0b0e2af6e88c590f826f15595d2313d498525a42
Andres Guzman-Ballen <acosmicflamingo@gmail.com>
2026-06-11 12:24:25 -0500

    BUG: Remove packaging dependency from numba_cuda._compat (#114)
    * BUG: Remove packaging dependency from numba_cuda._compat
    
    * Generate version tuple via list comprehension approach

--- src/numba_cuda_mlir/numba_cuda/_compat.py
+++ src/numba_cuda_mlir/numba_cuda/_compat.py
@@ -1,9 +1,9 @@
 # SPDX-FileCopyrightText: Copyright (c) 2025 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
 # SPDX-License-Identifier: BSD-2-Clause
-from packaging import version
 from cuda import core
 
-
-CUDA_CORE_VERSION = version.parse(core.__version__)
-CUDA_CORE_GT_0_6 = CUDA_CORE_VERSION >= version.parse("0.6.0")
-CUDA_CORE_GE_1_0 = CUDA_CORE_VERSION >= version.parse("1.0.0")
+# core.__version__ returns str like '1.0.1',
+# '1.0.2.dev46+g16df11dab', etc.
+CUDA_CORE_VERSION = tuple(int(i) for i in core.__version__.split(".") if i.isdigit())
+CUDA_CORE_GT_0_6 = CUDA_CORE_VERSION >= (0, 6, 0)
+CUDA_CORE_GE_1_0 = CUDA_CORE_VERSION >= (1, 0, 0)
