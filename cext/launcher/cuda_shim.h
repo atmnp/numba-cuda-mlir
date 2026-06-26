5ff15bec9ef2bc014bb7c2458773bc7e45a92020
Yevhenii Havrylko <yhavrylko@nvidia.com>
2026-06-26 16:17:34 -0400

    Add full support for big dynamic smem (#148)

--- cext/launcher/cuda_shim.h
+++ cext/launcher/cuda_shim.h
@@ -49,6 +49,7 @@ typedef enum {
     CU_DEVICE_ATTRIBUTE_MAX_GRID_DIM_Z              = 7,
     CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR     = 75,
     CU_DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR     = 76,
+    CU_DEVICE_ATTRIBUTE_MAX_SHARED_MEMORY_PER_BLOCK_OPTIN = 97,
 } CUdevice_attribute;
 
 /* ---- pointer / function attributes ------------------------------------- */
@@ -58,6 +59,7 @@ typedef enum {
 } CUpointer_attribute;
 
 typedef enum {
+    CU_FUNC_ATTRIBUTE_SHARED_SIZE_BYTES = 1,
     CU_FUNC_ATTRIBUTE_MAX_DYNAMIC_SHARED_SIZE_BYTES = 8,
 } CUfunction_attribute;
 
